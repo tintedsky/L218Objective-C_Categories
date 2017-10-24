2e9c47ba550b4ad61c6083b9d2a820599eb9520a
tintedsky <hongbonew@gmail.com>
2017-10-24 15:39:20 -0400

    Initial Commit

---
+++ L218Objective-C-Categories/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  L218Objective-C-Categories
+//
+//  Created by Hongbo Niu on 2017-10-24.
+//  Copyright © 2017 Udemy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
